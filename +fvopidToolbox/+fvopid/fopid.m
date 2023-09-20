9776da6bd7471bf0d9b278668d675f9c5570c3c6
poziablo <92685029+poziablo@users.noreply.github.com>
2023-09-20 11:30:26 +0200

    Update fopid.m
    Fixed the bug with the initial FOPID condition set to 1 instead of 0

--- +fvopidToolbox/+fvopid/fopid.m
+++ +fvopidToolbox/+fvopid/fopid.m
@@ -16,7 +16,7 @@ function u = fopid(e, kp, ki, kd, vi, vd, h, N, saturationEnabled, saturationLim
         
         % Initialize input buffer
         inptuSignalBuffer = zeros(1, N);
-        inptuSignalBuffer(1) = 1;
+        inptuSignalBuffer(1) = 0;
         
         % Initialize step counter
         stepCounter = 1;
